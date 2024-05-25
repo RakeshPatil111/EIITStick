2d1f8cac4a8cc9555c3d1bf437c09bc7e23f5ded
rakesh patil <rakesh.patil4471@gmail.com>
2024-05-25 11:32:29 +0530

    Sample push

--- app/src/main/java/com/android/stickerpocket/presentation/MoreStickersFragment.kt
+++ app/src/main/java/com/android/stickerpocket/presentation/MoreStickersFragment.kt
@@ -1,6 +1,7 @@
 package com.android.stickerpocket.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,6 +26,7 @@ class MoreStickersFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         stickerSectionListAdapter = StickerSectionListAdapter()
         stickerSectionListAdapter.updateList(sectionStickers, requireActivity())
+        Log.d("TAG", "onViewCreated: ${sectionStickers.size}")
         binding.apply {
             rvStickerSection.content = GPHContent.trendingGifs
             rvStickerSection.fixedSizeCells = true

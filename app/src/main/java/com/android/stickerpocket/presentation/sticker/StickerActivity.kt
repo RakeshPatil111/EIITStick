fb41071db81fef18ed3167c947842f81aa52d1c7
prasann <Native.dev95@gmail.com>
2024-06-25 23:42:01 +0530

    AppoDeal  keyboard visiblity fixed

--- app/src/main/java/com/android/stickerpocket/presentation/sticker/StickerActivity.kt
+++ app/src/main/java/com/android/stickerpocket/presentation/sticker/StickerActivity.kt
@@ -1,7 +1,9 @@
 package com.android.stickerpocket.presentation.sticker
 
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.Toast
@@ -10,6 +12,7 @@ import android.window.OnBackInvokedDispatcher
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.FileProvider
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
@@ -150,6 +153,23 @@ class StickerActivity : AppCompatActivity() {
         // Bind the BannerView to Appodeal
         Appodeal.setBannerViewId(R.id.banner_view)
         Appodeal.show(this, Appodeal.BANNER_VIEW)
+
+
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.root)
+        constraintLayout.viewTreeObserver.addOnGlobalLayoutListener {
+            val r = Rect()
+            constraintLayout.getWindowVisibleDisplayFrame(r)
+            val screenHeight = constraintLayout.rootView.height
+            val keypadHeight = screenHeight - r.bottom
+
+            if (keypadHeight > screenHeight * 0.15) {
+                // Keyboard is opened
+                binding.bottomNavigation.visibility = View.GONE
+            } else {
+                // Keyboard is closed
+                binding.bottomNavigation.visibility = View.VISIBLE
+            }
+        }
 
     }
 

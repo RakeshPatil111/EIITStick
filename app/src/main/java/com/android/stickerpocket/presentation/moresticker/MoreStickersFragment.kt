70a9ea11564b75b70db5dba623ff6d5a34df63b8
Akshay-Kondekar <akshaykondekar81194@gmail.com>
2024-07-27 11:13:39 +0530

    added sticker search config dialog, recent search view and tenor recyclerview

--- app/src/main/java/com/android/stickerpocket/presentation/moresticker/MoreStickersFragment.kt
+++ app/src/main/java/com/android/stickerpocket/presentation/moresticker/MoreStickersFragment.kt
@@ -1,8 +1,10 @@
-package com.android.stickerpocket.presentation
+package com.android.stickerpocket.presentation.moresticker
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,13 +14,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.android.stickerpocket.databinding.FragmentSearchStickerBinding
+import com.android.stickerpocket.presentation.dialog.StickerConfigDialog
+import com.android.stickerpocket.presentation.dialog.StickerDownloadDialog
 import com.android.stickerpocket.presentation.sticker.StickerViewModel
 import com.android.stickerpocket.utils.StickerExt.stickerDTO
 import com.giphy.sdk.core.models.Media
 import com.giphy.sdk.ui.pagination.GPHContent
 import com.giphy.sdk.ui.views.GPHGridCallback
 
-class MoreStickersFragment : Fragment() {
+class MoreStickersFragment : Fragment(), StickerConfigDialog.StickerConfigDialogListener {
 
     private lateinit var binding: FragmentSearchStickerBinding
     private val viewModel by viewModels<StickerViewModel>()
@@ -75,16 +79,32 @@ class MoreStickersFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         interactor.initObserver(viewLifecycleOwner, viewModel)
         binding.apply {
-            rvStickerSection.content = GPHContent.trendingGifs
-            rvStickerSection.fixedSizeCells = true
+            rvGiphyStickerSection.content = GPHContent.trendingGifs
+            rvGiphyStickerSection.fixedSizeCells = true
 
-            rvStickerSection.callback = object : GPHGridCallback {
+            rvGiphyStickerSection.callback = object : GPHGridCallback {
                 override fun contentDidUpdate(resultCount: Int) {}
 
                 override fun didSelectMedia(media: Media) {
                     interactor.onStickerClick(media)
                 }
             }
+
+            ibtnConfigSticker.setOnClickListener {
+                val rect = Rect()
+                it.getGlobalVisibleRect(rect)
+                val x = rect.width() / 2
+                val y = rect.bottom - it.height
+                StickerConfigDialog.newInstance(x, y).show(childFragmentManager, StickerConfigDialog.TAG)
+            }
         }
+    }
+
+    override fun onGiphyStatusChange(status: Boolean) {
+        Log.d("on giphy switch change :"," $status")
+    }
+
+    override fun onTenorStatusChange(status: Boolean) {
+        Log.d("on tenor switch change :"," $status")
     }
 }
